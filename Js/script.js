c7ef9112a97f04a2af7dd14a543cb05f10b19915
Ekaterina <kattren990703@gmail.com>
2024-10-07 00:58:01 +0500

    added pages

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+let cityCard = document.getElementById('Limassol');
+function showInfo() {
+    let city = document.getElementsByTagName('area');
+    if(city == 'Limassol')
+    {
+        cityCard.style.display = 'flex';
+    }
+    
+}
