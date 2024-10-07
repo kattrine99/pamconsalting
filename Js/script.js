ded614c301b373e8fcd180d2b162d98e24a94687
Ekaterina <kattren990703@gmail.com>
2024-10-07 06:44:54 +0500

    changes

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,15 @@
-let cityCard = document.getElementById('Limassol');
-function showInfo() {
-    let city = document.getElementsByTagName('area');
-    if(city == 'Limassol')
-    {
-        cityCard.style.display = 'flex';
-    }
-    
-}
+// Получаем элементы
+const menuIcon = document.getElementById('menuIcon');
+const menuOverlay = document.getElementById('menuOverlay');
+const closeIcon = document.getElementById('closeIcon');
+
+// Открытие меню
+menuIcon.addEventListener('click', () => {
+    menuOverlay.classList.add('show');
+});
+
+// Закрытие меню
+closeIcon.addEventListener('click', () => {
+    menuOverlay.classList.remove('show');
+});
+
