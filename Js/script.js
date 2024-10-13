f410db964b6b2d0bacdbf2ccd280abd2da75558f
Ekaterina <kattren990703@gmail.com>
2024-10-13 16:43:33 +0500

    added new slides and sliders. add new pages

--- Js/script.js
+++ Js/script.js
@@ -1,10 +1,19 @@
+
+
 const hamburger = document.querySelector(".menu-icon");
 const navMenu = document.querySelector(".nav-list");
-
+navMenu.style.maxHeight = "0px";
 
 hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    if(navMenu.style.maxHeight == "0px")
+    {
+        navMenu.style.maxHeight = "300px";
+    }
+    else{
+        navMenu.style.maxHeight = "0px";
+    }
 });
 
 function showCityInfo(cityId) {
