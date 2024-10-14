0f82c4fa6255277f0067b56249fffa0ff02b6b77
Ekaterina <kattren990703@gmail.com>
2024-10-14 21:56:18 +0500

    added addaptable map code

--- Js/script.js
+++ Js/script.js
@@ -15,6 +15,38 @@ hamburger.addEventListener('click', ()=>{
         navMenu.style.maxHeight = "0px";
     }
 });
+window.onload = function() {
+    makeMapResponsive();
+    window.addEventListener('resize', makeMapResponsive);
+};
+
+function makeMapResponsive() {
+    var img = document.querySelector('.MapImg');
+    var originalWidth = 681; 
+    var originalHeight = 414; 
+    var newWidth = img.clientWidth;
+    var newHeight = img.clientHeight;
+    
+    var widthRatio = newWidth / originalWidth;
+    var heightRatio = newHeight / originalHeight;
+
+    updateMapArea('Paphos', [50,280,80], widthRatio, heightRatio);
+    updateMapArea('Limassol', [133, 310, 80], widthRatio, heightRatio);
+    updateMapArea('Nicosia', [179, 239, 80], widthRatio, heightRatio);
+    updateMapArea('Larnaka', [265, 286, 80], widthRatio, heightRatio);
+}
+
+function updateMapArea(areaName, coords, widthRatio, heightRatio) {
+    var area = document.querySelector(`area[alt='${areaName}']`);
+    var scaledCoords = coords.map((coord, index) => {
+        if (index % 2 === 0) {
+            return Math.round(coord * widthRatio);
+        } else {
+            return Math.round(coord * heightRatio);
+        }
+    });
+    area.coords = scaledCoords.join(',');
+}
 
 function showCityInfo(cityId) {
 
